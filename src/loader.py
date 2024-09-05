0b912b2b87be9e0c4fbf473dce8504dc27ff7936
Andjela Dimitrijevic <andjeladimi98@gmail.com>
2024-09-05 17:19:06 -0400

    Plugges skull-stripping + registration pipeline

--- src/loader.py
+++ src/loader.py
@@ -1,7 +1,9 @@
 import os
 import numpy as np
 import pandas as pd
+import shutil
 import glob
+import time
 from itertools import combinations
 # import nibabel as nib
 # from PIL import Image
@@ -130,133 +132,7 @@ class BCP(Dataset):
 
     def __len__(self):
         return len(self.index_combination)
-
-# Function to perform registration
-def perform_registration(mov, fix):
-    # Placeholder for actual registration code
-    # e.g., using an image processing library like SimpleITK or ANTs
-    print(f"Registering {mov} to {fix}")
-    # Return the result of registration
-    return f"registered_{image_1}_to_{image_2}"
-
-# Function to skull-strip an image
-def skull_strip(image_path, save_path):
-    # Placeholder for actual skull-stripping code
-    # e.g., using an image processing library like FSL or ANTs
-    print(f"Skull-stripping {image_path}")
-    os.system(f'/usr/local/fsl/bin/bet {image_path} ' \
-          f'{save_path}PS14_053_corrected_brain -f 0.8 -g 0')
-    os.system(f'/usr/local/fsl/bin/bet {save_path} ' \
-          f'{save_path} -f 0.3 -g 0')
     
-
-def preprocess_CP(filename, path):
-    
-    # Correct way to read the TSV data into a pandas DataFrame
-    df = pd.read_csv(f'{path}/{filename}', sep='\t')
-
-    # Filter to keep only subjects with >=3 timepoints
-    data_T1w_long_subjects = df.groupby('sub_id_bids').filter(lambda x: len(x) >= 3)
-
-    # Count the number of subjects with >=3 timepoints
-    HClongsubjects = np.unique(data_T1w_long_subjects['sub_id_bids'])
-
-    # train/val/test
-    totalN = len(HClongsubjects)
-    trainN = int(totalN*0.8)
-    permutedsubjects = HClongsubjects[np.random.permutation(range(totalN))]
-
-    traintest = np.zeros(totalN).astype('str')
-    traintest[:trainN] = 'train'
-    traintest[trainN:] = 'test'
-    # assign trainvlatest
-    demo_trainvaltest = np.zeros(len(data_T1w_long_subjects)).astype('str')
-    for i in range(len(permutedsubjects)):
-        indices = np.where(data_T1w_long_subjects['sub_id_bids'] == permutedsubjects[i])[0]
-        demo_trainvaltest[indices] = traintest[i]
-
-    data_T1w_long_subjects['traintest'] = demo_trainvaltest
-    
-
-    # set(data_T1w_long_subjects['sub_id_bids'][data_T1w_long_subjects['traintest'] == 'val'])\
-    #     .intersection(set(data_T1w_long_subjects['sub_id_bids'][data_T1w_long_subjects['traintest'] == 'test']))
-    
-    # data_T1w_long_subjects.to_csv('./data/CP/CP-healthy-longitudinal.csv')
-
-    # # Filter to keep only subjects with >3 sessions
-    # subjects_with_more_than_3_sessions = df.groupby('sub_id_bids').filter(lambda x: len(x) > 3)
-
-    # # Count the number of trios that can be formed for each subject
-    # trio_count = subjects_with_more_than_3_sessions.groupby('sub_id_bids').apply(lambda x: len(list(combinations(x['session'], 3)))).sum()
-    
-    # # Count the number of subjects that have exactly 3 time points
-    # subjects_with_exactly_3_sessions = df.groupby('sub_id_bids').filter(lambda x: len(x) == 3).sub_id_bids.nunique()
-
-    # Initialize a list to store the new rows
-    new_rows = []
-    trio_number = 1  # Initialize a counter for trio numbering
-
-    # Iterate over each subject
-    for sub_id, group in data_T1w_long_subjects.groupby('sub_id_bids'):
-        if len(group) == 3:
-            # If the subject has exactly 3 timepoints, just sort them by age
-            sorted_trio = group.sort_values(by='age')
-            sorted_trio['trio_id'] = f'trio-{trio_number:03d}'  # Assign trio number
-            new_rows.extend(sorted_trio.itertuples(index=False))
-            trio_number += 1  # Increment the trio number
-        elif len(group) > 3:
-            # Generate all possible trios of sessions for subjects with more than 3 sessions
-            for trio in combinations(group.itertuples(index=False), 3):
-                # Sort the trio by age
-                sorted_trio = sorted(trio, key=lambda x: x.age)
-                # Convert the sorted trio to a DataFrame
-                sorted_trio_df = pd.DataFrame(sorted_trio)
-                sorted_trio_df.columns = group.columns  # Maintain the original column names
-                sorted_trio_df['trio_id'] = f'trio-{trio_number:03d}'  # Assign trio number
-                new_rows.extend(sorted_trio_df.itertuples(index=False))
-                trio_number += 1  # Increment the trio number
-
-    # Create a new DataFrame from the new rows
-    trios_df = pd.DataFrame(new_rows, columns=list(data_T1w_long_subjects.columns) + ['trio_id'])
-
-
-    # Save the new DataFrame to a CSV file
-    trios_df.to_csv("./data/CP/trios_sorted_by_age.csv", index=False)
-    # Rigid reg to middle time point
-
-    # Iterate through each subject in the DataFrame
-    for sub_id, group in trios_df.groupby('sub_id_bids'):
-        # Since the data is grouped by subject, each group represents a subject with multiple trios
-        trios = [group.iloc[i:i+3] for i in range(0, len(group), 3)]
-        
-        for trio in trios:
-            # Extract the paths for the trio
-            # /home/andjela/joplin-intra-inter/work_dir/reg_n4_wdir/10006/PS14_053/wf/n4/PS14_053_corrected
-            path_1 = trio.iloc[0]['scan_id']
-            path_2 = trio.iloc[1]['scan_id']  # This is the reference
-            path_3 = trio.iloc[2]['scan_id']
-
-            # Create a directory to save the preprocessed images if it doesn't exist already, create the directory
-            if not os.path.exists(f'./data/CP/{sub_id}/{trio.iloc[1]["trio_id"]}'):
-                os.makedirs(f'./data/CP/{sub_id}/{trio.iloc[1]["trio_id"]}')
-
-            save_path = f'./data/CP/{sub_id}/{trio.iloc[1]["trio_id"]}/'
-            # Skull-strip each N4 corrected scan with 2 times bet command
-            scan_1 = skull_strip(path_1, save_path)
-            scan_2 = skull_strip(path_2, save_path)
-            scan_3 = skull_strip(path_3, save_path)
-
-            # Save scan_2 in dedicated space (./data/CP/sub_id_bids/trio_id/)
-            
-            # Perform registration on scan_1 -> scan_2 and scan_3 -> scan_2
-            reg_result_1_to_2 = perform_registration(scan_1, scan_2)
-            reg_result_3_to_2 = perform_registration(scan_3, scan_2)
-
-            # Save the registration results in dedicated space (./data/CP/sub_id_bids/trio_id/)
-
-
-
-
 def preprocess_BCP(filename, path):
     
     # filename = 'HCPBaby_image03_xnat_T1wT2w_2022-01-26.xlsx'
@@ -442,6 +318,169 @@ def preprocess_BCP(filename, path):
     # # data_T1w_long_subjects_slice.fname = 'images/' + data_T1w_long_subjects['Site-ID'] + '_' + data_T1w_long_subjects['session-id'] + '.png'
     # # data_T1w_long_subjects_slice.to_csv(f'{outdir}/demo-healthy-longitudinal.csv')
 
+# Function to perform registration
+def perform_registration(mov_path, fix_path, scan_id_mov, scan_id_fix, save_path):
+    # Placeholder for actual registration code
+    # e.g., using an image processing library like SimpleITK or ANTs
+    print(f"Registering {os.path.basename(mov_path)} to {os.path.basename(fix_path)}")
+    os.system(f"antsRegistration --dimensionality 3 --float 0 " \
+            f"--output [ {save_path}/mov2fix_{scan_id_mov}_{scan_id_fix}, {save_path}/{scan_id_mov}.nii.gz] " \
+            f"--interpolation Linear " \
+            f"--winsorize-image-intensities [0.005,0.995] " \
+            f"--use-histogram-matching 1 " \
+            f"--write-composite-transform 1 " \
+            f"--transform Rigid[0.1] " \
+            f"--metric Mattes[ {fix_path}, {mov_path}, 1, 32, Regular, 0.3 ] " \
+            f"--convergence [500x250x100,1e-6,10] " \
+            f"--shrink-factors 4x2x1 " \
+            f"--smoothing-sigmas 2x1x0vox " \
+            f"--verbose 1"
+            )
+
+# Function to skull-strip an image
+def skull_strip(image_path, scan_id):
+    # Placeholder for actual skull-stripping code
+    # e.g., using an image processing library like FSL or ANTs
+    print(f"Skull-stripped {os.path.basename(image_path)}")
+    if scan_id != 'PS14_001':
+        os.system(f'/usr/local/fsl/bin/bet {image_path} ' \
+          f'{os.path.dirname(image_path)}/{scan_id}_skull.nii.gz -f 0.5 -g 0')
+    else:
+        os.system(f'/usr/local/fsl/bin/bet {image_path} ' \
+            f'{os.path.dirname(image_path)}/{scan_id}_skull_it_1.nii.gz -f 0.8 -g 0')
+        os.system(f'/usr/local/fsl/bin/bet {os.path.dirname(image_path)}/{scan_id}_skull_it_1.nii.gz ' \
+          f'{os.path.dirname(image_path)}/{scan_id}_skull.nii.gz -f 0.3 -g 0')
+    
+def create_df_CP(filename, path):
+    
+    # Correct way to read the TSV data into a pandas DataFrame
+    df = pd.read_csv(f'{path}/{filename}', sep='\t')
+
+    # Add a new column 'n4_path' with the specified path format
+    df['n4_path'] = df.apply(lambda row: f'/home/andjela/joplin-intra-inter/work_dir/reg_n4_wdir/{row.participant_id}/{row.scan_id}/wf/n4/{row.scan_id}_corrected.nii.gz', axis=1)
+
+    # Filter to keep only subjects with >=3 timepoints
+    data_T1w_long_subjects = df.groupby('sub_id_bids').filter(lambda x: len(x) >= 3)
+
+    # Count the number of subjects with >=3 timepoints
+    HClongsubjects = np.unique(data_T1w_long_subjects['sub_id_bids'])
+
+    # train/val/test
+    totalN = len(HClongsubjects)
+    trainN = int(totalN*0.8)
+    permutedsubjects = HClongsubjects[np.random.permutation(range(totalN))]
+
+    traintest = np.zeros(totalN).astype('str')
+    traintest[:trainN] = 'train'
+    traintest[trainN:] = 'test'
+    # assign trainvlatest
+    demo_trainvaltest = np.zeros(len(data_T1w_long_subjects)).astype('str')
+    for i in range(len(permutedsubjects)):
+        indices = np.where(data_T1w_long_subjects['sub_id_bids'] == permutedsubjects[i])[0]
+        demo_trainvaltest[indices] = traintest[i]
+
+    data_T1w_long_subjects['traintest'] = demo_trainvaltest
+    
+    # Save the new DataFrame to a CSV file
+    data_T1w_long_subjects.to_csv("./data/CP/ind_data.csv", index=False)
+    # set(data_T1w_long_subjects['sub_id_bids'][data_T1w_long_subjects['traintest'] == 'val'])\
+    #     .intersection(set(data_T1w_long_subjects['sub_id_bids'][data_T1w_long_subjects['traintest'] == 'test']))
+    
+    # data_T1w_long_subjects.to_csv('./data/CP/CP-healthy-longitudinal.csv')
+
+    # # Filter to keep only subjects with >3 sessions
+    # subjects_with_more_than_3_sessions = df.groupby('sub_id_bids').filter(lambda x: len(x) > 3)
+
+    # # Count the number of trios that can be formed for each subject
+    # trio_count = subjects_with_more_than_3_sessions.groupby('sub_id_bids').apply(lambda x: len(list(combinations(x['session'], 3)))).sum()
+    
+    # # Count the number of subjects that have exactly 3 time points
+    # subjects_with_exactly_3_sessions = df.groupby('sub_id_bids').filter(lambda x: len(x) == 3).sub_id_bids.nunique()
+
+    # Initialize a list to store the new rows
+    new_rows = []
+    trio_number = 1  # Initialize a counter for trio numbering
+
+    # Iterate over each subject
+    for sub_id, group in data_T1w_long_subjects.groupby('sub_id_bids'):
+        if len(group) == 3:
+            # If the subject has exactly 3 timepoints, just sort them by age
+            sorted_trio = group.sort_values(by='age')
+            sorted_trio['trio_id'] = f'trio-{trio_number:03d}'  # Assign trio number
+
+            # Add a 'path' column for each trio based on participant_id and scan_id
+            sorted_trio['path'] = sorted_trio.apply(
+                lambda row: f'/home/andjela/joplin-intra-inter/work_dir/reg_n4_wdir/{row.participant_id}/{row.scan_id}/wf/n4/{row.scan_id}_skull.nii.gz', axis=1)
+            new_rows.extend(sorted_trio.itertuples(index=False))
+            trio_number += 1  # Increment the trio number
+        elif len(group) > 3:
+            # Generate all possible trios of sessions for subjects with more than 3 sessions
+            for trio in combinations(group.itertuples(index=False), 3):
+                # Sort the trio by age
+                sorted_trio = sorted(trio, key=lambda x: x.age)
+                # Convert the sorted trio to a DataFrame
+                sorted_trio_df = pd.DataFrame(sorted_trio)
+                sorted_trio_df.columns = group.columns  # Maintain the original column names
+                sorted_trio_df['trio_id'] = f'trio-{trio_number:03d}'  # Assign trio number
+                sorted_trio_df['path'] = sorted_trio_df.apply(
+                    lambda row: f'/home/andjela/joplin-intra-inter/work_dir/reg_n4_wdir/{row.participant_id}/{row.scan_id}/wf/n4/{row.scan_id}_skull.nii.gz', axis=1)
+                new_rows.extend(sorted_trio_df.itertuples(index=False))
+                trio_number += 1  # Increment the trio number
+
+    # Create a new DataFrame from the new rows
+    trios_df = pd.DataFrame(new_rows, columns=list(data_T1w_long_subjects.columns) + ['trio_id', 'path'])
+
+    # Save the new DataFrame to a CSV file
+    trios_df.to_csv("./data/CP/trios_sorted_by_age.csv", index=False)
+
+    return df, trios_df
+
+def preprocess_CP(df, trios_df):
+    # Skull-strip each N4 corrected scan with 2 times bet command
+    start_time = time.time()
+    # Only to be done on participants which have multiple time points
+    for index, row in df.iterrows():
+        if row['scan_id'] in trios_df['scan_id'].values:
+            print(f'Scan nbr: {index}')
+            skull_strip(row.n4_path, row.scan_id)
+    end_time = time.time()
+
+    # Calculate the duration
+    duration = end_time - start_time
+
+    # Print the time taken
+    print(f"Time taken for skull stripping: {duration} seconds")
+    
+    # Rigid reg to middle time point
+    # Iterate through each subject in the DataFrame
+    for sub_id, group in trios_df.groupby('sub_id_bids'):
+        # Since the data is grouped by subject, each group represents a subject with multiple trios
+        trios = [group.iloc[i:i+3] for i in range(0, len(group), 3)]
+        
+        for trio in trios:
+            # Extract the paths for the trio
+            # /home/andjela/joplin-intra-inter/work_dir/reg_n4_wdir/10006/PS14_053/wf/n4/PS14_053_corrected
+            path_1 = trio.iloc[0]['path']
+            path_2 = trio.iloc[1]['path']  # This is the reference
+            path_3 = trio.iloc[2]['path']
+
+            # Create a directory to save the preprocessed images if it doesn't exist already, create the directory
+            if not os.path.exists(f'./data/CP/{sub_id}/{trio.iloc[1]["trio_id"]}'):
+                os.makedirs(f'./data/CP/{sub_id}/{trio.iloc[1]["trio_id"]}')
+
+            save_path = f'./data/CP/{sub_id}/{trio.iloc[1]["trio_id"]}/'
+
+            # Save scan_2 in dedicated space (./data/CP/sub_id_bids/trio_id/) if not already there
+            if not os.path.exists(f'{save_path}/{os.path.basename(path_2)}'):
+                shutil.copy(path_2, f'{save_path}/{trio.iloc[1]['scan_id']}.nii.gz')
+            
+            # Perform registration on scan_1 -> scan_2 and scan_3 -> scan_2
+            perform_registration(path_1, path_2, trio.iloc[0]['scan_id'], trio.iloc[1]['scan_id'], save_path)
+            perform_registration(path_3, path_2, trio.iloc[2]['scan_id'], trio.iloc[1]['scan_id'], save_path)
+            print('Done')
+            # os.path.dirname(image_path)
+
+
 
 
 def load_and_preprocess_data():
@@ -449,7 +488,8 @@ def load_and_preprocess_data():
     # Preprocess the data
     filename = 'all-participants.tsv'
     path = './data/CP'
-    data = preprocess_CP(filename, path)
+    ind_df, trios_data = create_df_CP(filename, path)
+    preprocess_CP(ind_df, trios_data)
     
     # # Create a tf.data.Dataset
     # dataset = tf.data.Dataset.from_tensor_slices(data)
